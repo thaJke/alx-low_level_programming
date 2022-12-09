839ab0c81381fb23e6af9cef874db128187b0a53
Thabo Jake Nyirenda <tjaynyirenda@gmail.com>
2022-12-09 09:13:47 -0800

    Code that prints out text using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Retrun: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
