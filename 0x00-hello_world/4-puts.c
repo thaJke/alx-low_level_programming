27f894560c6e347a5d0dcd0a136a101af2d51cc7
Thabo Jake Nyirenda <tjaynyirenda@gmail.com>
2022-12-09 08:40:45 -0800

    First C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle,");
+	puts("\n");
+	return(0);
+}
