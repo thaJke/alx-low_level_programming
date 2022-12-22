a8ce59ce75ad544d5e43e40b55ad62bf45d67ce6
Thabo Jake Nyirenda <tjaynyirenda@gmail.com>
2022-12-22 15:15:05 -0800

    prototpe function that joins string with limit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * _strncat - Joins to strings together
+ * @dest: The destination value
+ * @src: The source value
+ * @n: the limit of the concatinating
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *strncat(char *dest, char *src, int n)
+{
+	int len = 0, j = 0;
+
+	while (dest[len])
+	{
+		len++
+	}
+
+	while (j < n && src[j])
+	{
+		dest[len] = src[j];
+		len++;
+		j++
+	}
+
+	dest[len + n + 1] = '\0';
+
+	return (dest);
+}
